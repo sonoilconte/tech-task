8d81784f356a53af5877706d127b8053cc9d4e2a
sonoilconte <sepp.r.hammer@gmail.com>
2023-02-06 23:33:14 -0800

    Clean up language, currency settings

--- scripts/customI18nSettings.js
+++ scripts/customI18nSettings.js
@@ -1,9 +1,4 @@
 module.exports = {
-    locales: [
-        { code: 'en-US', file: 'en-US.js', iso: 'en-US' },    
-        { code: 'es', file: 'es.js', iso: 'es-ES' }
-    ],
-    defaultLocale: 'en-US',
     currency: 'USD',
     country: 'US',
     countries: [
@@ -11,9 +6,16 @@ module.exports = {
       { name: 'ES', label: 'Spain' },
     ],
     currencies: [
-      { name: 'EUR', label: 'Euro', iso: 'es-ES' },
-      { name: 'USD', label: 'Dollar', iso: 'en-US' }
+      { name: 'USD', label: 'Dollar', iso: 'en' },
+      { name: 'EUR', label: 'Euro', iso: 'es' },
     ],
+    // I think code on the locale must match commerce tools exactly —> en-US, es
+    locales: [
+        { code: 'en-US', file: 'en.js', iso: 'en' },    
+        { code: 'es', file: 'es.js', iso: 'es' },
+    ],
+    defaultLocale: 'en-US',
+    lazy: true,
     langDir: 'lang/',
     vueI18n: {
       fallbackLocale: 'en-US',
@@ -36,107 +38,3 @@ module.exports = {
     },
     reloadOnLanguageChange: process.env.RELOAD_ON_LANGUAGE_CHANGE !== 'false',
 };
-
-
-// module.exports = {
-//     currency: 'USD',
-//     country: 'US',
-//     countries: [
-//       { name: 'US', label: 'United States' },
-//       { name: 'ES', label: 'Spain' },
-//     ],
-//     currencies: [
-//       { name: 'EUR', label: 'Euro', iso: 'es' },
-//       { name: 'USD', label: 'Dollar', iso: 'en' }
-//     ],
-//     locales: [
-//       { code: 'en-US', label: 'American English', file: 'en-US.js', iso: 'en-US' },
-//       { code: 'es-ES', label: 'Spanish', file: 'es.js', iso: 'es' }
-//     ],
-//     defaultLocale: 'en-US',
-//     lazy: true,
-//     langDir: 'lang/',
-//     vueI18n: {
-//       fallbackLocale: 'en-US',
-//       numberFormats: {
-//         en: {
-//           currency: {
-//             style: 'currency', currency: 'USD', currencyDisplay: 'symbol'
-//           }
-//         },
-//         es: {
-//           currency: {
-//             style: 'currency', currency: 'EUR', currencyDisplay: 'symbol'
-//           }
-//         }
-//       }
-//     },
-//     detectBrowserLanguage: false,
-//     autoChangeCookie: {
-//       currency: process.env.MULTICURRENCY_ENABLED !== 'true'
-//     },
-//     reloadOnLanguageChange: process.env.RELOAD_ON_LANGUAGE_CHANGE !== 'false'
-//   };
-  
-
-
-// module.exports = {
-//   currency: 'USD',
-//   country: 'US',
-//   countries: [
-//     { name: 'US', label: 'United States' },
-//     { name: 'ES', label: 'Spain' }
-//   ],
-//   currencies: [
-//     { name: 'EUR', label: 'Euro' },
-//     { name: 'USD', label: 'US Dollar' }
-//   ],
-//   locales: [
-//     {
-//       code: 'en-US',
-//       label: 'American English',
-//       file: 'en-US.js',
-//       iso: 'en-US'
-//     },
-//     { code: 'es', label: 'Spanish', file: 'es.js', iso: 'es' }
-//   ],
-//   defaultLocale: 'en',
-//   lazy: true,
-//   langDir: 'lang/',
-//   vueI18n: {
-//     fallbackLocale: 'en',
-//     numberFormats: {
-//       en: {
-//         currency: {
-//           style: 'currency',
-//           currency: 'USD',
-//           currencyDisplay: 'symbol'
-//         }
-//       },
-//       de: {
-//         currency: {
-//           style: 'currency',
-//           currency: 'EUR',
-//           currencyDisplay: 'symbol'
-//         }
-//       },
-//       'en-US': {
-//         currency: {
-//           style: 'currency',
-//           currency: 'EUR',
-//           currencyDisplay: 'symbol'
-//         }
-//       },
-//       es: {
-//         currency: {
-//           style: 'currency',
-//           currency: 'ARS',
-//           currencyDisplay: 'symbol'
-//         }
-//       }
-//     }
-//   },
-//   detectBrowserLanguage: false,
-//   autoChangeCookie: { currency: true },
-//   reloadOnLanguageChange: false
-// };
